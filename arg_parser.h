457799457b40a01648e64b385259b5faa492966f
Robin Obůrka <robin.oburka@nic.cz>
2013-12-23 15:52:34 +0100

    Renumber all devices. Is not neccesary to have emty slots in configuration for pseudo devices

--- arg_parser.h
+++ arg_parser.h
@@ -3,9 +3,6 @@
 
 #include <stdbool.h>
 
-#define KW_INTEN	"intensity"
-#define KW_INLVL	"level"
-#define KW_ALL		"all"
 #define KW_WAN		"wan"
 #define KW_WIFI		"wifi"
 #define KW_PWR		"pwr"
@@ -15,23 +12,27 @@
 #define KW_LAN3		"lan3"
 #define KW_LAN4		"lan4"
 #define KW_LAN5		"lan5"
+#define KW_ALL		"all"
+#define KW_INTEN	"intensity"
+#define KW_INLVL	"level"
+#define KW_BINMASK	"binmask"
 #define KW_ENABLE	"enable"
 #define KW_DISABLE	"disable"
 #define KW_AUTO		"auto"
 
 #define DEV_UNDEF	-1
-#define DEV_INTEN	0
-#define DEV_INLVL	1
-#define DEV_ALL		2
-#define DEV_WAN		3
-#define DEV_WIFI	4
-#define DEV_PWR		5
-#define DEV_LAN		6
-#define DEV_LAN1	7
-#define DEV_LAN2	8
-#define DEV_LAN3	9
-#define DEV_LAN4	10
-#define DEV_LAN5	11
+#define DEV_WAN		0
+#define DEV_WIFI	1
+#define DEV_PWR		2
+#define DEV_LAN		3
+#define DEV_LAN1	4
+#define DEV_LAN2	5
+#define DEV_LAN3	6
+#define DEV_LAN4	7
+#define DEV_LAN5	8
+#define DEV_ALL		9
+#define DEV_INTEN	10
+#define DEV_INLVL	11
 
 #define ST_DISABLE	0
 #define ST_ENABLE	1
