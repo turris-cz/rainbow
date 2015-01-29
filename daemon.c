16f74c23a5daca6521b6b8f96d612bd212fff03a
Robin Obůrka <robin.oburka@nic.cz>
2015-01-29 16:03:44 +0100

    [smrt] First version of SMRT status indicator

--- daemon.c
+++ daemon.c
@@ -23,15 +23,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "daemon.h"
 #include "configuration.h"
+#include "arg_parser.h"
+#include "reg_setters.h"
 
 #define BUFFSIZE 512
 #define SLEEP_TIME_USEC 150000
 #define FLASH_DELAY 100000
 #define SNIFF_FILE_NET "/proc/net/dev"
+#define SNIFF_FILE_SMRT "/tmp/smrtd/eth2"
 #define MAX_LEN_INTERFACE_NAME 20
+#define MAX_LEN_STATUS 20
 #define WIFI_DEV_NAME "wlan0:"
 #define EPS 10
 
@@ -43,6 +48,49 @@ struct network_state {
 	unsigned long long int last_t;
 	bool wifi_active;
 };
+
+struct smrt_state {
+	int prev_state_cat;
+	time_t prev_start;
+	bool silent_second;
+};
+
+#define SMRT_STATE_OK 0
+#define SMRT_STATE_ERR 1
+#define SMRT_STATE_WAIT 2
+
+struct smrt_status_map {
+	const char *status;
+	int status_type;
+};
+
+static struct smrt_status_map status_map[] = {
+	{ "presence query", SMRT_STATE_WAIT },
+	{ "upload firmware", SMRT_STATE_WAIT },
+	{ "version query", SMRT_STATE_WAIT },
+	{ "config", SMRT_STATE_WAIT },
+	{ "activate", SMRT_STATE_WAIT },
+	{ "no signal", SMRT_STATE_ERR },
+	{ "reset", SMRT_STATE_WAIT },
+	{ "not present", SMRT_STATE_OK },
+	{ "idle", SMRT_STATE_ERR },
+	{ "handshake", SMRT_STATE_WAIT },
+	{ "training", SMRT_STATE_WAIT },
+	{ "online", SMRT_STATE_OK },
+	{ "CRC error", SMRT_STATE_ERR },
+	{ "disabled", SMRT_STATE_ERR },
+	{ NULL, 0 }
+};
+
+static int get_state_category(const char *state) {
+	for (size_t i = 0; status_map[i].status != NULL; i++) {
+		if (strcmp(state, status_map[i].status) == 0) {
+			return status_map[i].status_type;
+		}
+	}
+
+	return SMRT_STATE_ERR;
+}
 
 static bool iteration_network(volatile unsigned char *mem, struct network_state *state) {
 	char buffer[BUFFSIZE];
@@ -103,11 +151,94 @@ static bool iteration_network(volatile unsigned char *mem, struct network_state 
 	return true;
 }
 
+static bool iteration_smrt(volatile unsigned char *mem, struct smrt_state *ctx) {
+	(void)mem; (void) ctx;
+	char buffer[BUFFSIZE];
+	char status[MAX_LEN_STATUS];
+	int state_cat;
+
+#ifdef DEBUG
+	FILE *dbgf = fopen("/tmp/rainbow_debug", "a");
+#endif
+
+	FILE *f = fopen(SNIFF_FILE_SMRT, "r");
+	if (f == NULL) {
+		state_cat = SMRT_STATE_OK;
+	} else {
+
+		while (fgets(buffer, BUFFSIZE, f) != NULL) {
+			status[0] = '\0';
+			if (sscanf(buffer, "<status>%[a-zA-Z ]20s</status>", status) > 0)
+				break;
+		}
+		fclose(f);
+#ifdef DEBUG
+	fprintf(dbgf, "Obtained state: %s\n", status);
+#endif
+		state_cat = get_state_category(status);
+	}
+
+	long long int now = (long long int) time(NULL);
+	ctx->silent_second = (now % 2) ? true : false;
+#ifdef DEBUG
+	fprintf(dbgf, "Now: %llu\n", now);
+#endif
+
+	if (state_cat == SMRT_STATE_OK) {
+		if (ctx->prev_state_cat != SMRT_STATE_OK) {
+			set_status(mem, DEV_WAN, ST_AUTO);
+#ifdef DEBUG
+			fprintf(dbgf, "LED: AAAAAA\n");
+#endif
+		}
+
+	} else if (state_cat == SMRT_STATE_ERR) {
+		if (ctx->silent_second) {
+			set_status(mem, DEV_WAN, ST_DISABLE);
+#ifdef DEBUG
+			fprintf(dbgf, "LED: ______\n");
+#endif
+		} else {
+			set_status(mem, DEV_WAN, ST_ENABLE);
+#ifdef DEBUG
+			fprintf(dbgf, "LED: FFFFFF\n");
+#endif
+			usleep(FLASH_DELAY);
+			set_status(mem, DEV_WAN, ST_DISABLE);
+#ifdef DEBUG
+			fprintf(dbgf, "LED: ______\n");
+#endif
+		}
+	} else if (state_cat == SMRT_STATE_WAIT) {
+		if (ctx->silent_second) {
+			set_status(mem, DEV_WAN, ST_DISABLE);
+#ifdef DEBUG
+			fprintf(dbgf, "LED: ______\n");
+#endif
+		} else {
+			set_status(mem, DEV_WAN, ST_ENABLE);
+#ifdef DEBUG
+			fprintf(dbgf, "LED: FFFFFF\n");
+#endif
+		}
+	}
+
+	ctx->prev_state_cat = state_cat;
+
+#ifdef DEBUG
+	fclose(dbgf);
+#endif
+
+	return true;
+}
+
 void do_some_daemon_stuff(volatile unsigned char *mem) {
 	struct network_state network = { 0, 0, false };
+	struct smrt_state smrt = { SMRT_STATE_OK, 0, false };
 
 	while (true) {
 		iteration_network(mem, &network);
+		iteration_smrt(mem, &smrt);
 		usleep(SLEEP_TIME_USEC);
 	}
 }
